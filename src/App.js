0a56c2a5b37db6ac8dbb9e7506f10cc17a54ed36
bhattacharya0293 <bhattacharya0293@gmail.com>
2024-12-23 10:49:30 -0600

    dummy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello from App component</h1>;
+  const title = "Blog Post";
+  const body = "This is my blog post";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment Two" },
+    { id: 3, text: "Comment Three" },
+  ];
+
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments {comments.length}</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  return (
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
+    </div>
+  );
 };
 
 export default App;
